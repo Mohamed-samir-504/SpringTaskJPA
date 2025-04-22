adff4d3075fe9a3dc85a411098535e16d2635711
mtolba <mm.ahmedd504@gmail.com>
2025-04-22 14:03:54 +0200

    Add security configuration class

---
+++ src/main/java/org/example/springtaskjpa/SecurityConfig.java
@@ -0,0 +1,41 @@
+package org.example.springtaskjpa;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/add").hasRole("ADMIN")
+                        .requestMatchers("/update/**").hasAnyRole("ADMIN")
+                        .requestMatchers("/delete/**").hasAnyRole("ADMIN")
+                        .requestMatchers("/view/**", "/login", "/register").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin123")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
+    }
+}
